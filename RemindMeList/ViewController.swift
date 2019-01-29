5bdbaf892bdb21772f61fbe348f5abb74bfb204e
Beng Ee Chuah <bengeechuah@be-macbook-air.local>
2019-01-30 00:06:05 +0800

    Initial Commit

---
+++ RemindMeList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemindMeList
+//
+//  Created by Beng Ee Chuah on 30/01/2019.
+//  Copyright © 2019 Beng Ee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
