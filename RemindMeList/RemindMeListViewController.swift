98b935c6963a28c8b52813ed246d82600562b51a
Beng Ee Chuah <bengeechuah@be-macbook-air.local>
2019-02-05 00:06:32 +0800

    Add new item button

--- RemindMeList/RemindMeListViewController.swift
+++ RemindMeList/RemindMeListViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class RemindMeListViewController: UITableViewController {
     
     
-    let itemArray = ["Write Will", "Apply Visa", "Get Cash"]
+    var itemArray = ["Write Will", "Apply Visa", "Get Cash"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,5 +43,34 @@ class RemindMeListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    // Mark - Add New Item button IB outlet
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Remind Me Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what happened once the user click the uialert button
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+            
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
 }
 
