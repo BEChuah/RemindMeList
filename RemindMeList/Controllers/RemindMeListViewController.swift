bd7d849853acf9ad80c602bd7cbd3bf2ae565518
Beng Ee Chuah <bengeechuah@be-macbook-air.local>
2019-02-07 14:29:43 +0800

    Convert to MVC design

--- RemindMeList/Controllers/RemindMeListViewController.swift
+++ RemindMeList/Controllers/RemindMeListViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class RemindMeListViewController: UITableViewController {
     
     
-    var itemArray = ["Write Will", "Apply Visa", "Get Cash"]
+    var itemArray = [Item] ()
 
     let defaults = UserDefaults.standard
     
@@ -19,35 +19,63 @@ class RemindMeListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let items = defaults.array(forKey: "RemindMeListArray") as! [String]
-        itemArray = items
+        let newItem = Item()
+        newItem.title = "Find"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Love"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Here"
+        itemArray.append(newItem3)
+        
+      if let items = defaults.array(forKey: "RemindMeListArray") as? [Item] {
+           itemArray = items
+         }
+  
     }
 
 //MARK Tableview Datasource Method
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //Ternary operator ==>
+        // value = condition? valueIfTrue : valueIfFalse
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         
         return cell
     }
+    
+    
+    
     // Mark - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        // print(indexPath.row)
+        // print(indexPath.row)]
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            
-               tableView.cellForRow(at: indexPath)?.accessoryType = .none }
-        else{
-               tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark}
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    
+    
     
     // Mark - Add New Item button IB outlet
     
@@ -59,7 +87,13 @@ class RemindMeListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what happened once the user click the uialert button
-            self.itemArray.append(textField.text!)
+            
+            let newItem = Item()
+            
+            newItem.title = textField.text!
+            
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "RemindMeListArray")
             
---
+++ RemindMeList/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  item.swift
+//  RemindMeList
+//
+//  Created by Beng Ee Chuah on 06/02/2019.
+//  Copyright © 2019 Beng Ee. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    
+    var done:Bool = false
+}
